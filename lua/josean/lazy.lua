f6e2a8951f1435a5476c63604c7a8252cd14368d
Sahand Karimkhani <sahandkarimkhani@gmail.com>
2024-08-06 11:41:48 +0330

    [Add]: lsp server

--- lua/josean/lazy.lua
+++ lua/josean/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("josean.plugins", {
+require("lazy").setup({ { import = "josean.plugins" }, { import = "josean.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
